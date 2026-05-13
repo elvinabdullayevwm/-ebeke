5c15e3c7bada3ab6278a7972b587fdc468fb1115
elvinabdullayevwm <elvin.abdullayev.wm@gmail.com>
2026-05-13 22:16:56 +0400

    Update api.js

--- js/api.js
+++ js/api.js
@@ -2,17 +2,12 @@ const scriptURL = "BURA_OZ_SCRIPT_URL_NI_YAPISDIR";
 
 async function apiCall(data) {
     try {
-        // 'no-cors' sildik ki, serverin cavabını eşidə bilək
         const response = await fetch(scriptURL, {
             method: 'POST',
-            body: JSON.stringify(data) 
+            body: JSON.stringify(data)
         });
-
-        // Serverdən gələn mətni oxuyuruq
         const result = await response.text();
-        console.log("Serverin cavabı:", result);
-        return result; 
-
+        return result;
     } catch (error) {
         console.error("Şəbəkə xətası:", error);
         return "error";
