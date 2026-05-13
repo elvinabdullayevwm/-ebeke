00a779da0700cc697807c51045bec57ba462dfa7
elvinabdullayevwm <elvin.abdullayev.wm@gmail.com>
2026-05-13 22:17:28 +0400

    Update ui-controller.js

--- js/ui-controller.js
+++ js/ui-controller.js
@@ -1,12 +1,13 @@
 function showSection(id) {
-    const sections = ['login-section', 'register-section', 'verify-section', 'menu-section', 'order-section', 'trip-section', 'marketplace-section'];
+    const sections = ['login-section', 'register-section', 'marketplace-section'];
     sections.forEach(s => {
         const el = document.getElementById(s);
         if(el) el.style.display = 'none';
     });
+    
     const target = document.getElementById(id);
     if(target) {
         target.style.display = 'block';
-        target.classList.add('fade-in'); // animations.css-dəki effekti çağırır
+        target.classList.add('fade-in');
     }
 }
