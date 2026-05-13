2d162034677de0cc5e8277a63cf60ec4c51282d9
elvinabdullayevwm <elvin.abdullayev.wm@gmail.com>
2026-05-13 21:37:10 +0400

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,10 @@
+const scriptURL = "BURA_OZ_SCRIPT_URL_NI_YAPISDIR";
 
+async function apiCall(data) {
+    const response = await fetch(scriptURL, {
+        method: 'POST',
+        mode: 'no-cors',
+        body: JSON.stringify(data)
+    });
+    return response;
+}
